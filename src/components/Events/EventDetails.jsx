0327f08b6c760e396a1ec609b4b471206092ddcb
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-09-12 12:46:45 +0300

    feat: displayed event details using tanstack query

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,8 +1,22 @@
-import { Link, Outlet } from 'react-router-dom';
+import {Link, Outlet, useParams} from 'react-router-dom';
 
 import Header from '../Header.jsx';
+import {useQuery} from "@tanstack/react-query";
+import {fetchEvent} from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
+  const { id } = useParams();
+
+  const{
+      data,
+      isError,
+      error
+  } = useQuery({
+    queryKey: ['event', id],
+    queryFn: ({signal}) => fetchEvent({id, signal})
+  });
+
   return (
     <>
       <Outlet />
@@ -12,24 +26,34 @@ export default function EventDetails() {
         </Link>
       </Header>
       <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
+        {data && (
+            <>
+              <header>
+                <h1>{data.title}</h1>
+                <nav>
+                  <button>Delete</button>
+                  <Link to="edit">Edit</Link>
+                </nav>
+              </header>
+              <div id="event-details-content">
+                  <img src={`http://localhost:3000/${data.image}`} alt={data.title}/>
+                  <div id="event-details-info">
+                      <div>
+                      <p id="event-details-location">{data.location}</p>
+                    <time dateTime={`Todo-DateT$Todo-Time`}>{data.time}</time>
+                  </div>
+                  <p id="event-details-description">{data.description}</p>
+                </div>
+              </div>
+            </>
+        )}
       </article>
+      {isError &&
+          <ErrorBlock
+              title="An error occured"
+              message={error.info?.message || 'Failed to fetch events.'}
+          />
+      }
     </>
   );
 }
