48e6eddf7479170be3de0a7d0f5512bfe7cdaa08
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-09-12 10:34:08 +0300

    refactor code to use dynamic query functions & query keys

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -1,14 +1,52 @@
-import { useRef } from 'react';
+import {useRef, useState} from 'react';
+import {useQuery} from "@tanstack/react-query";
+import {fetchEvents} from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import EventItem from "./EventItem.jsx";
 
 export default function FindEventSection() {
   const searchElement = useRef();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const {
+    data,
+    isPending,
+    isError,
+    error
+  } = useQuery({
+    queryKey: ['events', { search: searchTerm }],
+    queryFn: ({signal}) => fetchEvents({signal, searchTerm}),
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
+    setSearchTerm(searchElement.current.value);
+  }
+
+  let content = <p>Please enter a search term and to find events.</p>;
+
+  if(isPending) {
+    content = <LoadingIndicator/>;
+  }
+
+  if(isError) {
+    content = <ErrorBlock
+        title="An error occured"
+        message={error.info?.message || 'Failed to fetch events.'}
+    />;
+  }
+
+  if(data) {
+    content = <ul>
+      {data.map(event => <li key={event.id}>
+        <EventItem event={event}/>
+      </li>)}
+    </ul>
   }
 
   return (
-    <section className="content-section" id="all-events-section">
+      <section className="content-section" id="all-events-section">
       <header>
         <h2>Find your next event!</h2>
         <form onSubmit={handleSubmit} id="search-form">
@@ -20,7 +58,7 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+        {content}
     </section>
   );
 }
